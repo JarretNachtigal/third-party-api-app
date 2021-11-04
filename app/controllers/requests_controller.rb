9ad7a84954188a4cd8156ef3fec9f0ac8ec1c455
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-04 11:23:22 -0400

    homework

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   def create
-    
+    response = HTTP.get("http://api.airvisual.com/v2/countries?key=#{Rails.application.credentials.weather_key}")
+    render json: response.as_json
   end
 end
